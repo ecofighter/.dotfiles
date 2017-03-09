b367b8d6b0b68c5788bdacc5de1229e7cd8c8c7a
ecofighter <syotaa1@gmail.com>
2017-03-10 04:26:18 +0900

    refactared

--- scripts/notify-mute.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-notify-send -a pulseaudio -t 1000 "Mute Toggled" "<big>$(pactl list sinks | grep '^[[:space:]]Mute:' | tail -n 1)</big>"
--- scripts/notify-volume.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-notify-send -a pulseaudio -t 1000 "Volume Changed" "<big>"$(pactl list sinks | grep "^[[:space:]]Volume:" | tail -n 1 | sed -e "s,.* \([0-9][0-9]*\)%.*,\1,")%"</big>"
