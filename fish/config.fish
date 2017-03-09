7d5e7cda516cb365803d28a27b5e7cc59258939f
ecofighter <syotaa1@gmail.com>
2017-03-09 22:55:56 +0900

    make prompt theme use powerline font

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,6 @@
 set -g theme_color_scheme zenburn
 set -g theme_display_vi yes
+set -g theme_powerline_fonts yes
+set -g theme_project_dir_length 2
 set -U FZF_LEGACY_KEYBINDINGS 0
 fish_vi_key_bindings
