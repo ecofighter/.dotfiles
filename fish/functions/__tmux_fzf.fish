5040861d6685c42c6e610258defddb9ccd730f71
ecofighter <syotaa1@gmail.com>
2017-03-10 19:39:14 +0900

    set Esc to cancel

--- fish/functions/__tmux_fzf.fish
+++ fish/functions/__tmux_fzf.fish
@@ -1,5 +1,4 @@
 function __tmux_fzf
-  #tmux-start.sh
   set -l sessions (tmux list-sessions)
   if [ -z "$sessions" ];
     tmux new-session
@@ -7,9 +6,10 @@ function __tmux_fzf
     return
   end
   set -l create_new "Create New Session"
-  set sessions $create_new $sessions
-  for x in $sessions; echo $x; end | fzf | cut -d: -f1 | read select
-  if [ $select = $create_new ];
+  for x in $create_new $sessions; echo $x; end | fzf | cut -d: -f1 | read select
+  if [ -z "$select" ];
+    return
+  else if [ $select = $create_new ];
     tmux new-session
   else if [ -n "$select" ];
     tmux attach-session -t "$select"
